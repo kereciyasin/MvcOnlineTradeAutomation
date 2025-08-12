cbe0d8fea0c41a61cce0f49a9a114e9751e6cadb
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 09:42:24 +0200

    feat(models): add simple Customer entity without validations

---
+++ MvcOnlineTradeAutomation/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        public string Email { get; set; }
+    }
+}
