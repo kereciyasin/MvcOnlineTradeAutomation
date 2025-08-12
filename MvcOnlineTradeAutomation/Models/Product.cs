42e3d7faed7c7bbfa2439af9c28e9fad324bede8
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 09:39:20 +0200

    feat(models): create Category and Product entities

---
+++ MvcOnlineTradeAutomation/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Brand { get; set; }
+
+        public short Stock { get; set; }
+
+        public decimal PurchasePrice { get; set; }
+
+        public decimal SalePrice { get; set; }
+
+        public bool Status { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
