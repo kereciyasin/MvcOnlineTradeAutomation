b3c3aa3400066fa754ce3c3e253c55e49bce6dfb
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 10:31:42 +0200

    chore: install EntityFramework and add initial DbContext with all entities

---
+++ MvcOnlineTradeAutomation/Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using MvcOnlineTradeAutomation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<SalesTransaction> SalesTransactions { get; set; }
+
+
+    }
+}
