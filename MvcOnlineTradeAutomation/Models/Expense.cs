8156ca8c4ab8e6833e3fc8ec5b4190dc1bf16f92
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 09:49:51 +0200

    feat(models): add Expense entity

---
+++ MvcOnlineTradeAutomation/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int ExpenseID { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
