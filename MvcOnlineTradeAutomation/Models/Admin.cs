f72a93f86ba4b535187aed3b14b43915cdcc2321
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 09:48:38 +0200

    feat(models): add InvoiceItem and Admin entities

---
+++ MvcOnlineTradeAutomation/Models/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
---
+++ MvcOnlineTradeAutomation/Models/InvoiceItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class InvoiceItem
+    {
+        [Key]
+        public int InvoiceItemID { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
