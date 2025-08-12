b3d560fa44f79413ded20e1fcf9a1217ba4db1bd
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 09:40:56 +0200

    feat(models): add Employee entity

---
+++ MvcOnlineTradeAutomation/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
