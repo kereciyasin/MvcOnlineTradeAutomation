a2e429135a416849904044cf0e7f844a84807fbf
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 10:34:20 +0200

    chore(db): run initial automatic migration and seed database

---
+++ MvcOnlineTradeAutomation/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MvcOnlineTradeAutomation.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcOnlineTradeAutomation.Data.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MvcOnlineTradeAutomation.Data.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
