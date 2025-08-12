80dfadf77ff36ae062dae60dbed0f3d5e266e87d
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-12 09:45:41 +0200

    feat(models): add Invoice entity

---
+++ MvcOnlineTradeAutomation/Models/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTradeAutomation.Models
+{
+    public class Invoice
+    {
+        [Key]
+        public int InvoiceID { get; set; }
+
+        public string InvoiceSerialNumber { get; set; }
+
+        public string InvoiceSequenceNumber { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string TaxOffice { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string DeliveredBy { get; set; }
+
+        public string ReceivedBy { get; set; }
+    }
+}
