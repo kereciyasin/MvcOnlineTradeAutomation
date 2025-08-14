273dd4526166ff110f4ea9bef692593dbac95ed7
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-14 15:10:58 +0200

    fix(product): ensure Index action returns product list with category data and update view with null checks

---
+++ MvcOnlineTradeAutomation/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using MvcOnlineTradeAutomation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradeAutomation.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var products = db.Products.Include("Category").ToList();
+            return View(products);
+        }
+    }
+}
