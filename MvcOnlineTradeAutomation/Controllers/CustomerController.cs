3ebdc230fd9cd3aa12338d39a65bfe1b531e9b33
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-17 22:33:17 +0200

    fix(customer): correct CreateCustomer view (model, fields, and post action)

---
+++ MvcOnlineTradeAutomation/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using MvcOnlineTradeAutomation.Data;
+using MvcOnlineTradeAutomation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradeAutomation.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var values = db.Customers.ToList();
+            return View(values);
+        }
+        public ActionResult CreateCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCustomer(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
