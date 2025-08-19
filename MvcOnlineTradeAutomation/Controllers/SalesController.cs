a9dbaff5906751b04610b76b9c25767ee76ded85
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-19 22:31:59 +0200

    ...

---
+++ MvcOnlineTradeAutomation/Controllers/SalesController.cs
@@ -0,0 +1,20 @@
+﻿using MvcOnlineTradeAutomation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradeAutomation.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var sales = db.SalesTransactions.ToList();
+            return View(sales);
+        }
+    }
+}
