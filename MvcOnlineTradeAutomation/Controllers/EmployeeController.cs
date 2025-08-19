373efb85abfb88110429075ab6fe90fee66c4280
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-19 21:44:52 +0200

    feat(employee): simplify list actions to Details and Update (remove Delete)

---
+++ MvcOnlineTradeAutomation/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using MvcOnlineTradeAutomation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradeAutomation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Context db = new Context();
+        public ActionResult Index()
+        {
+            var values = db.Employees.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateEmployee(Models.Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
