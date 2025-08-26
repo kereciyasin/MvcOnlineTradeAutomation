0d5158e84efeeee91e55c4371d19a9a7b33649f2
Yasin Kereci <yke144907@stud.gibb.ch>
2025-08-26 09:56:07 +0200

    feat(sales): add Print button to SalesDetails view and update headings to English
    - Changed page title and breadcrumb from Turkish to English ("Sales Details")
    - Added row-level Print button for each sales transaction
    - Added global "Print Page" button to print the entire table
    - Improved table alignment and Bootstrap styling

--- MvcOnlineTradeAutomation/Controllers/SalesController.cs
+++ MvcOnlineTradeAutomation/Controllers/SalesController.cs
@@ -52,5 +52,50 @@ namespace MvcOnlineTradeAutomation.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult UpdateSale(int id)
+        {
+            var sale = db.SalesTransactions.Find(id);
+            List<SelectListItem> products = (from x in db.Products.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.ProductName,
+                                                 Value = x.ProductID.ToString()
+                                             }).ToList();
+            List<SelectListItem> customers = (from x in db.Customers.ToList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.FirstName + " " + x.LastName,
+                                                  Value = x.CustomerID.ToString()
+                                              }).ToList();
+            List<SelectListItem> employees = (from x in db.Employees.ToList()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x.FirstName + " " + x.LastName,
+                                                  Value = x.EmployeeID.ToString()
+                                              }).ToList();
+            ViewBag.products = products;
+            ViewBag.customers = customers;
+            ViewBag.employees = employees;
+            return View("UpdateSale", sale);
+        }
+        public ActionResult UpdateSale(SalesTransaction s)
+        {
+            var sale = db.SalesTransactions.Find(s.SalesTransactionID);
+            sale.ProductID = s.ProductID;
+            sale.CustomerID = s.CustomerID;
+            sale.EmployeeID = s.EmployeeID;
+            sale.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            sale.Quantity = s.Quantity;
+            sale.Price = s.Price;
+            sale.TotalAmount = s.TotalAmount;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SalesDetails(int id)
+        {
+            var value = db.SalesTransactions.Where(x => x.SalesTransactionID == id).ToList();
+            return View(value);
+        }
+
     }
 }
