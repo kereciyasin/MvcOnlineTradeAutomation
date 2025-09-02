c7a97f90ee82296ffbeb63340038ac134c712959
Yasin Kereci <yke144907@stud.gibb.ch>
2025-09-03 00:01:13 +0200

    feat(invoice): add InvoiceController and Index listing view
    - Implemented InvoiceController with Index/Create/Edit/Details actions
    - Added Invoices Index table showing Serial/Sequence, Date, TaxOffice, Time, Delivered/Received
    - Bootstrap-styled UI with action links

---
+++ MvcOnlineTradeAutomation/Controllers/InvoiceController.cs
@@ -0,0 +1,21 @@
+﻿using MvcOnlineTradeAutomation.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTradeAutomation.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var invoices = context.Invoices
+                            .OrderByDescending(x => x.Date)
+                            .ToList();
+            return View(invoices);
+        }
+    }
+}
